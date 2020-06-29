afa3e9ee0726de27a12b2b346ff66b0cd306ee7f
bestfriend777 <59232292+bestfriend777@users.noreply.github.com>
2020-06-29 17:03:40 -0400

    adam

---
+++ Assets/Scripts/Behaviors/npc/peaceful/verticalWalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verticalWalk : MonoBehaviour
+{
+    bool walkingUp = true;
+    float walkSpeed = 5.0f;
+    int maxDistance = 10;
+    int currentDistance = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += gameObject.transform.forward * walkSpeed * Time.deltaTime;
+        
+        if(currentDistance > maxDistance)
+        {
+            walkSpeed = walkSpeed * -1;
+        }
+    }
+}
